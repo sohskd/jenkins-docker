bccba5f637acbce121ca62bd8c91e5578712fbf1
Desmond Soh See Kiat <sseekiat@dso.org.sg>
2019-10-07 11:28:16 +0800

    first commit

---
+++ src/main/java/com/example/jenkinsdockeragain/JenkinsDockerAgainApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsdockeragain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerAgainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerAgainApplication.class, args);
+	}
+
+}
